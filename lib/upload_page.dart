d4a31f04e0cb6ec4f4ac82e40f6b4930990cda6b
Sarvesh22904 <149946527+Sarvesh22904@users.noreply.github.com>
2024-03-25 15:14:09 +0530

    Changes in upload_page.dart

--- lib/upload_page.dart
+++ lib/upload_page.dart
@@ -3,6 +3,14 @@ import 'package:flutter/material.dart';
 class UploadPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // Function to be called when the button is pressed
+    void onUploadButtonPressed() {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Upload button pressed')),
+      );
+      // Here you can add your file upload functionality
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -11,8 +19,16 @@ class UploadPage extends StatelessWidget {
         ),
         backgroundColor: Colors.orange,
       ),
-      body: const Center(
-        child: Text('Welcome to the Upload Page!'),
+      body: Center(
+        child: ElevatedButton(
+          onPressed:
+              onUploadButtonPressed, // Call the function when the button is pressed
+          child: Text('Upload'),
+          style: ElevatedButton.styleFrom(
+            primary: Colors.orange, // Button background color
+            onPrimary: Colors.white, // Button text color
+          ),
+        ),
       ),
     );
   }
